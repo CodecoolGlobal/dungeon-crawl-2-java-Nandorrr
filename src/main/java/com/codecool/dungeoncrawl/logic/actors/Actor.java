8029cde5597f67cb95a825973b1237ff3b66d7b2
csnelly90 <csnelly90@gmail.com>
2022-05-23 19:06:58 +0200

    add validation to move method in Actor class

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -1,11 +1,14 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.CellType;
 import com.codecool.dungeoncrawl.logic.Drawable;
 
 public abstract class Actor implements Drawable {
     private Cell cell;
     private int health = 10;
+
+    private Boolean hasKey = false;
 
     public Actor(Cell cell) {
         this.cell = cell;
@@ -14,9 +17,12 @@ public abstract class Actor implements Drawable {
 
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
-        cell.setActor(null);
-        nextCell.setActor(this);
-        cell = nextCell;
+        CellType nextCellType = nextCell.getType();
+        if ((nextCellType == CellType.FLOOR && nextCell.getActor() == null) || (nextCellType == CellType.DOOR && hasKey)) {
+            cell.setActor(null);
+            nextCell.setActor(this);
+            cell = nextCell;
+        }
     }
 
     public int getHealth() {
@@ -33,5 +39,9 @@ public abstract class Actor implements Drawable {
 
     public int getY() {
         return cell.getY();
+    }
+
+    public void addKey() {
+        this.hasKey = true;
     }
 }
