6599c3c1d028925379a2da834b1d59feebfa49cf
csnelly90 <csnelly90@gmail.com>
2022-05-23 23:56:59 +0200

    add inventory field to Player class

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -1,10 +1,18 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.items.Item;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player extends Actor {
+
+    private List<Item> inventory;
+
     public Player(Cell cell) {
         super(cell);
+        inventory = new ArrayList<>();
     }
 
     public String getTileName() {
