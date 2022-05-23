1514b46d844f66bab82e2218099da4d7f625ce55
csnelly90 <csnelly90@gmail.com>
2022-05-24 01:00:55 +0200

    add getInventoryContentText method to Player class

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -2,9 +2,12 @@ package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.items.Item;
+import com.codecool.dungeoncrawl.logic.items.general.Key;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Player extends Actor {
 
@@ -20,6 +23,20 @@ public class Player extends Actor {
 
     public List<Item> getInventory() {
         return this.inventory;
+    }
+
+    public String getInventoryContentText() {
+        StringBuilder str = new StringBuilder();
+        Set<Item> inventoryItems = new HashSet<>(this.inventory);
+
+        for (Item item: inventoryItems) {
+            String itemName = item.getClass().getSimpleName();
+            int itemCount = countItemInInventory(itemName);
+            str.append(itemName);
+            str.append(": " + itemCount + " piece(s)");
+            str.append("\n");
+        }
+        return str.toString();
     }
 
     private int countItemInInventory(String itemName) {
