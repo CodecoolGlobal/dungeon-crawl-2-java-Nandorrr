94a3795607415684bb2bd480c031082b9ceade45
csnelly90 <csnelly90@gmail.com>
2022-05-24 00:16:53 +0200

    implement ActorStats enum

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/ActorStats.java
@@ -0,0 +1,17 @@
+package com.codecool.dungeoncrawl.logic.actors;
+
+public enum ActorStats {
+
+    PLAYER(100, 10, 20);
+
+    public int health;
+    public int damage;
+    public int baseArmor;
+
+    ActorStats(int health, int damage, int baseArmor) {
+        this.health = health;
+        this.damage = damage;
+        this.baseArmor = baseArmor;
+    }
+
+}
