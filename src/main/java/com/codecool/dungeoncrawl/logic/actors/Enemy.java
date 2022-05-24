e41daad81fc331631e149bfd502c457776594f3a
Nandorrr <87696275+Nandorrr@users.noreply.github.com>
2022-05-24 14:00:40 +0200

    add hitPlayer method to Skeleton to decrease players help if latters is around.

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Enemy.java
@@ -0,0 +1,7 @@
+package com.codecool.dungeoncrawl.logic.actors;
+
+public interface Enemy {
+
+    void hitPlayer();
+
+}
--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -2,7 +2,10 @@ package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
 
-public class Skeleton extends Actor {
+import java.util.List;
+import java.util.Random;
+
+public class Skeleton extends Actor implements Enemy{
     public Skeleton(Cell cell) {
         super(cell);
         this.health = ActorStats.SKELETON.health;
@@ -14,4 +17,17 @@ public class Skeleton extends Actor {
     public String getTileName() {
         return "skeleton";
     }
+
+    @Override
+    public void hitPlayer() {
+        List<Cell>  surroundingCells = super.getSurroundingCells();
+
+        for(Cell cell : surroundingCells){
+            if (cell.getActor() != null){
+                int hitDamage = ActorStats.SKELETON.damage;
+                cell.getActor().getHurt(hitDamage);
+            }
+        }
+    }
+
 }
