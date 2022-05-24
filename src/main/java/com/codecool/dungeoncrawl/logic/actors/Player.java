9d84a7b710dac5f0bbf6e1687eca2cd2f14e2995
csnelly90 <csnelly90@gmail.com>
2022-05-24 03:43:28 +0200

    implement pickUpItem and getInventoryContentText methods in Player class

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -2,7 +2,10 @@ package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.items.Item;
+import com.codecool.dungeoncrawl.logic.items.armors.ChestPlate;
 import com.codecool.dungeoncrawl.logic.items.general.Key;
+import com.codecool.dungeoncrawl.logic.items.potions.HealthPotion;
+import com.codecool.dungeoncrawl.logic.items.weapons.Sword;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -27,21 +30,69 @@ public class Player extends Actor {
 
     public String getInventoryContentText() {
         StringBuilder str = new StringBuilder();
-        Set<Item> inventoryItems = new HashSet<>(this.inventory);
+        List<String> inventoryItemNames = new ArrayList<>();
 
-        for (Item item: inventoryItems) {
-            String itemName = item.getClass().getSimpleName();
-            int itemCount = countItemInInventory(itemName);
-            str.append(itemName);
+        for (Item item: this.inventory) {
+            String itemName = item.getTileName();
+            inventoryItemNames.add(itemName);
+        }
+
+        Set<String> itemNamesNoDuplicate = new HashSet<>(inventoryItemNames);
+
+        for (String name: itemNamesNoDuplicate) {
+            int itemCount = countItemInInventory(name);
+            str.append("  - " + name);
             str.append(": " + itemCount + " piece(s)");
             str.append("\n");
         }
+
         return str.toString();
     }
 
     private int countItemInInventory(String itemName) {
-        int itemCount = (int) inventory.stream().filter(item -> item.getTileName().equalsIgnoreCase(itemName)).count();
-        return itemCount;
+        return (int) inventory.stream().filter(item -> item.getTileName().equalsIgnoreCase(itemName)).count();
+    }
+
+    public void pickUpItem() {
+        List<Cell> surroundingCells = getSurroundingCells();
+
+        for (Cell neighbor: surroundingCells) {
+            Item item = neighbor.getItem();
+            if (item != null) {
+                if (item instanceof Key) {
+                    hasKey = true;
+                    inventory.add(item);
+                } else if (item instanceof HealthPotion) {
+                    increaseHealth(((HealthPotion) item).getHealingValue());
+                    inventory.add(item);
+                } else if (item instanceof ChestPlate) {
+                    increaseArmor(((ChestPlate) item).increaseArmor());
+                    inventory.add(item);
+                } else if (item instanceof Sword) {
+                    increaseDamage(((Sword) item).getDamage());
+                    inventory.add(item);
+                } else {
+                    inventory.add(item);
+                }
+            }
+        }
+
+    }
+
+    private void increaseHealth(int extraHealth) {
+        if (this.health <= 80) {
+            this.health += extraHealth;
+        } else {
+            this.health = 100;
+        }
+    }
+
+    private void increaseDamage(int extraDamage) {
+        this.damage += extraDamage;
+    }
+
+    private void increaseArmor(int extraArmor) {
+        this.armor += extraArmor;
     }
 
     private void removeFromInventory(String itemName) {
