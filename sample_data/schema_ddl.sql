e83c6091520357c7d4ba622d7b1aca17b204d4fe
Nandorrr <87696275+Nandorrr@users.noreply.github.com>
2022-06-07 15:43:08 +0200

    add custom db schema

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -1,19 +1,38 @@
-DROP TABLE IF EXISTS public.game_state;
-CREATE TABLE public.game_state (
+DROP TABLE IF EXISTS player;
+DROP TABLE IF EXISTS inventory;
+DROP TABLE IF EXISTS game_state;
+
+
+CREATE TABLE player (
+                        id SERIAL PRIMARY KEY,
+                        player_name text NOT NULL,
+                        health integer NOT NULL,
+                        armor integer NOT NULL ,
+                        damage integer NOT NULL
+
+
+);
+
+
+CREATE TABLE inventory (
+                      id serial PRIMARY KEY,
+                      player_id INTEGER REFERENCES player(id),
+                      item_name text NOT NULL,
+                      item_value int NOT NULL
+
+);
+
+CREATE TABLE game_state (
     id serial NOT NULL PRIMARY KEY,
     current_map text NOT NULL,
     saved_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     player_id integer NOT NULL
 );
 
-DROP TABLE IF EXISTS public.player;
-CREATE TABLE public.player (
-    id serial NOT NULL PRIMARY KEY,
-    player_name text NOT NULL,
-    hp integer NOT NULL,
-    x integer NOT NULL,
-    y integer NOT NULL
-);
 
-ALTER TABLE ONLY public.game_state
-    ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
+ALTER TABLE ONLY game_state
+    ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES player(id);
+
+
+ALTER TABLE ONLY inventory
+    ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES player(id);
