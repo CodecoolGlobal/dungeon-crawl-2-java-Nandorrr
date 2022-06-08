e4c0b0018eeb30dff6368eee484431ee1345b898
csnelly90 <csnelly90@gmail.com>
2022-06-08 13:10:09 +0200

    add skeleton to GameDatabaseManager class

--- src/main/java/com/codecool/dungeoncrawl/dao/GameDatabaseManager.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameDatabaseManager.java
@@ -1,23 +1,59 @@
 package com.codecool.dungeoncrawl.dao;
 
 import com.codecool.dungeoncrawl.logic.actors.Player;
+import com.codecool.dungeoncrawl.logic.items.Item;
+import com.codecool.dungeoncrawl.model.GameState;
+import com.codecool.dungeoncrawl.model.InventoryModel;
 import com.codecool.dungeoncrawl.model.PlayerModel;
 import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.List;
 
 public class GameDatabaseManager {
     private PlayerDao playerDao;
+    private InventoryDao inventoryDao;
+    private GameStateDao gameStateDao;
 
     public void setup() throws SQLException {
         DataSource dataSource = connect();
         playerDao = new PlayerDaoJdbc(dataSource);
+        inventoryDao = new InventoryDaoJdbc(dataSource, playerDao);
+        gameStateDao = new GameStateDaoJdbc(dataSource, playerDao);
     }
 
     public void savePlayer(Player player) {
         PlayerModel model = new PlayerModel(player);
         playerDao.add(model);
+    }
+
+    public void saveInventory(List<Item> inventory) {
+        InventoryModel model = new InventoryModel(inventory);
+        inventoryDao.add(model);
+    }
+
+    public void saveGameState() {
+        // TODO: implement method
+    }
+
+    public void updateSavedGame() {
+        // TODO: implement method
+    }
+
+    public List<GameState> getAllGameState() {
+        return gameStateDao.getAll();
+    }
+
+    public boolean doesPlayerExist(String playerName) {
+        List<PlayerModel> players = playerDao.getAll();
+
+        for (PlayerModel player : players) {
+            if (player.getPlayerName().equals(playerName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private DataSource connect() throws SQLException {
