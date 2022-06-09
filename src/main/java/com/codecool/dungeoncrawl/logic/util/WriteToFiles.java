ed29c99ee46376208934f2078a17a212937a8768
Nandorrr <87696275+Nandorrr@users.noreply.github.com>
2022-06-09 14:51:53 +0200

    add writetofiles class to create and write the map in the new file

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/util/WriteToFiles.java
@@ -0,0 +1,30 @@
+package com.codecool.dungeoncrawl.logic.util;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+
+public class WriteToFiles {
+    public String fileCreater(String date) throws IOException {
+        String filePath = "/Users/pohlnandor/Documents/GitHub/OOP Projects/dungeon-crawl-2-java-Nandorrr" +
+                "/src/main/resources/savedMaps/" + date + "txt";
+        new File(filePath);
+
+        return filePath;
+
+    }
+
+    public void  writeInTheFile (String map, String filePath) throws IOException {
+        try{
+        Writer fileWriter = new OutputStreamWriter(new FileOutputStream(filePath), StandardCharsets.UTF_8);
+        fileWriter.write(map);
+        fileWriter.close();
+        System.out.println("Map write to txt was successful");
+        } catch (IOException e){
+            System.out.println("Problem with file writing");
+            throw new IOException();
+        }
+
+    }
+
+}
